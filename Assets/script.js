b51816cf0c6c684c5e4398e1737ca41fd2e8ade7
Luis Moore <luismoore@Luiss-MacBook-Air.local>
2023-12-26 12:57:24 +0000

    added JS and updated HTML

--- Assets/script.js
+++ Assets/script.js
@@ -13,24 +13,46 @@ function dateAndTime() {
 }
 setInterval(dateAndTime, 1000);
 
-//
+// Main function - local storage , save button , colour changes .
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  var currentHour = dayjs().format('H');
+
+  function colorBlock() {  
+    $('.time-block').each(function() {  
+      var blockHour = $(this).attr("id").split("hour-")[1]; 
+      console.log("currentHour" + currentHour)
+      console.log("blockHour" + blockHour)
+      if (blockHour == currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+       } else if (blockHour < currentHour) {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      } else if (blockHour > currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+  });
+  
+  // Function to save input to local storage , when save button is clicked
+  $('.saveBtn').on('click', function() {
+    var key = $(this).parent().attr('id');
+    var value = $(this).siblings('.description').val();
+    localStorage.setItem(key, value);
+  });
+
+  $('.time-block').each(function() {
+    var key = $(this).attr('id');
+    var value = localStorage.getItem(key);
+    $(this).children('.description').val(value);
+  });
+}
+
+// Colour Changing
+ 
+colorBlock();
+
 });
